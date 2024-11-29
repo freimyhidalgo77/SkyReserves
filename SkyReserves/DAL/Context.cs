70ccd54b02656868dcea9eb531464281201adc67
fahh7 <fahh7@FREIMYHP>
2024-11-29 11:05:18 -0400

    namespace context

--- SkyReserves/DAL/Context.cs
+++ SkyReserves/DAL/Context.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection.Emit;
 using Microsoft.EntityFrameworkCore;
 
+namespace SkyReserves.DAL;
 public class Context : DbContext
 {
     public Context(DbContextOptions<Context> options) : base(options) { }
