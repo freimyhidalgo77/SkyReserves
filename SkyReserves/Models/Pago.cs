c949f6b580f6d1e59599ea966cf61cc8214c4435
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-12-08 07:41:21 -0400

    Modificacines

--- SkyReserves/Models/Pago.cs
+++ SkyReserves/Models/Pago.cs
@@ -4,23 +4,26 @@ using System.ComponentModel.DataAnnotations;
 namespace SkyReserves.Models
 {
     public class Pago
-    {
+    { 
 
-        [Key]
+        public int PagoId { get; set; } 
 
-        public int PagoId { get; set; }
 
-        [Required(ErrorMessage = "Favor seleccione una forma de pago.")]
-        public string? MetodoPago { get; set; } //Forma de pago paar la reserva de vuelo
+        [Required(ErrorMessage = "La clase de vuelo es obligatoria.")]
+        public int ClaseVueloId { get; set; }
 
-        public DateTime FechaPago { get; set; } = DateTime.Now;
+        [Required(ErrorMessage = "El número de tarjeta es obligatorio.")]
+        public string TarjetaNumero { get; set; }
 
-        [Required(ErrorMessage = "Favor marque el estado del pago.")]
-        public string? EstadoPago { get; set; } //(Pagado/No pagado)
+        [Required(ErrorMessage = "La fecha de vencimiento es obligatoria.")]
+        public string FechaVencimiento { get; set; }
 
-        [ForeignKey("Reserva")]
-        public int ReservaId { get; set; }
-        Reserva? Reserva { get; set; }
+        [Required(ErrorMessage = "El CVV es obligatorio.")]
+        [StringLength(4, ErrorMessage = "El CVV debe tener entre 3 y 4 dígitos.", MinimumLength = 3)]
+        public string CVV { get; set; }
 
+        [Required(ErrorMessage = "El CVV es obligatorio.")]
+        public double MontoPagar { get; set; }
     }
+
 }
